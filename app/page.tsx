709a913fd70393b274205a1e1406d25a1c1c2066
eullergomes <eullertexeira@gmail.com>
2025-01-29 17:27:25 -0300

    feat: implement dashboard and profile pages; refactor main app layout with Chakra UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,34 @@
-"use client";
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
 
-import React, { useEffect, useRef } from 'react';
-import '@r2u/javascript-ar-sdk';
-
-declare global {
-  interface Window {
-    R2U: any;
+const App = () => {
+  
+  const boxStyles = {
+    p: "10px",
+    bg: "green.500",
+    color: "white",
+    m: "10px",
+    textAlign: "center",
+    '&:hover': {
+      bg: "blue.500"
+    }
   }
-}
-
-const ARViewer = () => {
-  const arButtonRef = useRef(null);
-  const viewerRef = useRef(null);
-
-  useEffect(() => {
-    (async () => {
-      const { R2U } = window;
-      if (!R2U) {
-        console.error('R2U SDK não carregado corretamente.');
-        return;
-      }
-
-      await R2U.init({ customerId: '9755e2a9-379b-45d7-af75-30a5e7c5dabc' });
-      const sku = 'HLC0040';
-      const isActive = await R2U.sku.isActive(sku);
-
-      if (isActive) {
-        if (arButtonRef.current) {
-          await R2U.ar.attach({
-            element: arButtonRef.current,
-            sku,
-          });
-        }
-
-        if (viewerRef.current) {
-          await R2U.viewer.create({
-            element: viewerRef.current,
-            sku,
-          });
-        }
-      }
-    })();
-  }, []);
 
   return (
-    <div>
-      <button ref={arButtonRef}>Ver AR</button>
-      <div ref={viewerRef} style={{ width: '50%', height: '500px' }}></div>
-    </div>
-  );
-};
+    <Container as="section" className="mx-auto my-10" textAlign={"center"}>
+      <Heading className="text-4xl" fontWeight="bold">Chakra UI Components</Heading>
 
-export default ARViewer;
+      <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>
+      <Text color="blue.300" fontWeight="bolder">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>
+    
+      <Box bg="orange.500" color="white" p={4} mt={4}>
+        <Text>This is a box</Text>
+      </Box>
+
+      <Box {...boxStyles}> 
+        Hello Euller
+      </Box>
+    </Container>
+  )
+}
+ 
+export default App;
